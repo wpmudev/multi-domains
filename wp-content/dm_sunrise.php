baeef645c4654e6bebf88028116d60d5988b6f27
ulrich <ulrich@f53fe46e-8c02-459b-9fdd-93e75771b278>
2010-11-09 08:46:58 +0000

    Multi-Domains don't display php error when sunrise.php does not find any multi-domain
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/multi-domains@983 f53fe46e-8c02-459b-9fdd-93e75771b278

--- wp-content/dm_sunrise.php
+++ wp-content/dm_sunrise.php
@@ -29,30 +29,32 @@ $wpdb->suppress_errors( false );
 if ( !$mapped_id && preg_replace( "/^www\./", "", DOMAIN_CURRENT_SITE ) !== preg_replace( "/^www\./", "", $using_domain ) ) {
 	$md_domains = unserialize( $wpdb->get_var( "SELECT meta_value FROM {$wpdb->sitemeta} WHERE meta_key = 'md_domains' AND site_id = 1" ) );
 
-	if( $_SERVER['REQUEST_URI'] == '/' ) {
-		foreach ( $md_domains as $domain ) {
+	if( is_array( $md_domains ) ) {
+		if( $_SERVER['REQUEST_URI'] == '/' ) {
+			foreach ( $md_domains as $domain ) {
 
-			if( $_SERVER['HTTP_HOST'] == strtolower( $domain['domain_name'] ) ) {
-				$location = 'http://' . DOMAIN_CURRENT_SITE;
+				if( $_SERVER['HTTP_HOST'] == strtolower( $domain['domain_name'] ) ) {
+					$location = 'http://' . DOMAIN_CURRENT_SITE;
 
-				if ( strpos( $_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS' ) == true || strpos( $_SERVER['SERVER_SOFTWARE'], 'ExpressionDevServer' ) == true ) {
-					header( "Refresh: 0;url=$location ");
-				} else {
-					if ( php_sapi_name() != 'cgi-fcgi' )
-						status_header($status); // This causes problems on IIS and some FastCGI setups
-					header( "Location: $location", true, 301 );
+					if ( strpos( $_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS' ) == true || strpos( $_SERVER['SERVER_SOFTWARE'], 'ExpressionDevServer' ) == true ) {
+						header( "Refresh: 0;url=$location ");
+					} else {
+						if ( php_sapi_name() != 'cgi-fcgi' )
+							status_header($status); // This causes problems on IIS and some FastCGI setups
+						header( "Location: $location", true, 301 );
+					}
+
+					die;
 				}
 
-				die;
 			}
-
 		}
-	}
 
-	foreach( $md_domains as $domain ) {
-		if ( preg_match( '|' . strtolower( $domain['domain_name'] ) . '$|', preg_replace( "/^www\./", "", $using_domain ) ) ) {
-			define( 'COOKIE_DOMAIN', '.' . strtolower( $domain['domain_name'] ) );
-			break;
+		foreach( $md_domains as $domain ) {
+			if ( preg_match( '|' . strtolower( $domain['domain_name'] ) . '$|', preg_replace( "/^www\./", "", $using_domain ) ) ) {
+				define( 'COOKIE_DOMAIN', '.' . strtolower( $domain['domain_name'] ) );
+				break;
+			}
 		}
 	}
 }
