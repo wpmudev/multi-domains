2c26916dbaa87af68af9857bb8f6488ce3783825
ulrich <ulrich@f53fe46e-8c02-459b-9fdd-93e75771b278>
2010-11-12 20:03:11 +0000

    Fixes Domain Mapping support broken in Multi-Domains plugin
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/multi-domains@1009 f53fe46e-8c02-459b-9fdd-93e75771b278

--- wp-content/dm_sunrise.php
+++ wp-content/dm_sunrise.php
@@ -1,8 +1,7 @@
 <?php
 
-if ( defined( 'COOKIE_DOMAIN' ) ) {
+if ( defined( 'COOKIE_DOMAIN' ) )
 	die( 'The constant "COOKIE_DOMAIN" is defined (probably in wp-config.php). Please remove or comment out that define() line.' );
-}
 
 // Compatibility mode
 define('DM_COMPATIBILITY', 'yes');
@@ -26,35 +25,33 @@ $mapped_id = $wpdb->get_var( "SELECT blog_id FROM {$wpdb->dmtable} WHERE domain 
 
 $wpdb->suppress_errors( false );
 
-if ( !$mapped_id && preg_replace( "/^www\./", "", DOMAIN_CURRENT_SITE ) !== preg_replace( "/^www\./", "", $using_domain ) ) {
+if ( !$mapped_id && ( preg_replace( "/^www\./", "", DOMAIN_CURRENT_SITE ) !== preg_replace( "/^www\./", "", $using_domain ) ) ) {
 	$md_domains = unserialize( $wpdb->get_var( "SELECT meta_value FROM {$wpdb->sitemeta} WHERE meta_key = 'md_domains' AND site_id = 1" ) );
 
-	if( is_array( $md_domains ) ) {
-		if( $_SERVER['REQUEST_URI'] == '/' ) {
-			foreach ( $md_domains as $domain ) {
+	if( $_SERVER['REQUEST_URI'] == '/' ) {
+		foreach ( $md_domains as $domain ) {
 
-				if( $_SERVER['HTTP_HOST'] == strtolower( $domain['domain_name'] ) ) {
-					$location = 'http://' . DOMAIN_CURRENT_SITE;
+			if( $_SERVER['HTTP_HOST'] == strtolower( $domain['domain_name'] ) ) {
+				$location = 'http://' . DOMAIN_CURRENT_SITE;
 
-					if ( strpos( $_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS' ) == true || strpos( $_SERVER['SERVER_SOFTWARE'], 'ExpressionDevServer' ) == true ) {
-						header( "Refresh: 0;url=$location ");
-					} else {
-						if ( php_sapi_name() != 'cgi-fcgi' )
-							status_header($status); // This causes problems on IIS and some FastCGI setups
-						header( "Location: $location", true, 301 );
-					}
-
-					die;
+				if ( strpos( $_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS' ) == true || strpos( $_SERVER['SERVER_SOFTWARE'], 'ExpressionDevServer' ) == true ) {
+					header( "Refresh: 0;url=$location ");
+				} else {
+					if ( php_sapi_name() != 'cgi-fcgi' )
+						status_header($status); // This causes problems on IIS and some FastCGI setups
+					header( "Location: $location", true, 301 );
 				}
 
+				die;
 			}
-		}
 
-		foreach( $md_domains as $domain ) {
-			if ( preg_match( '|' . strtolower( $domain['domain_name'] ) . '$|', preg_replace( "/^www\./", "", $using_domain ) ) ) {
-				define( 'COOKIE_DOMAIN', '.' . strtolower( $domain['domain_name'] ) );
-				break;
-			}
+		}
+	}
+
+	foreach( $md_domains as $domain ) {
+		if ( preg_match( '|' . strtolower( $domain['domain_name'] ) . '$|', preg_replace( "/^www\./", "", $using_domain ) ) ) {
+			define( 'COOKIE_DOMAIN', '.' . strtolower( $domain['domain_name'] ) );
+			break;
 		}
 	}
 }
